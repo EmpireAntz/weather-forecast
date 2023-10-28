58415aa023c29b6d216e3d66ae505dc8ba44ce70
EmpireAntz <thebigboognish@gmail.com>
2023-10-27 21:08:35 -0700

    removed html for the basic cards to render them in with js. added a function to add the cards with data via longitude and latitiude and im also logging all the data in the console to keep track of everything happening. next i need to use goecoding to be able to search by city to display the results as well as use local storage to save recently searched cities.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 var openWeatherAPIKey = 'a0d3f129408d5612ac665943f7214076'
 var openWeatherBaseURL = 'https://api.openweathermap.org/data/2.5/forecast?'
 var geocodingBaseURL = 'http://api.openweathermap.org/geo/1.0/direct?q='
-var lat = 20
+var lat = 49
 var lon = 10
 var fullURL = (`${openWeatherBaseURL}lat=${lat}&lon=${lon}&appid=${openWeatherAPIKey}`)
 
@@ -12,5 +12,40 @@ fetch(fullURL)
 
     .then (function(data){
         console.log(data)
+        addWeatherCards(data)
     })
 
+function addWeatherCards(data) {
+    var cardContainer = document.getElementById('weather-card-container')
+
+    for (i= 0; i < 5; i++) {
+        var fiveDayData = data.list[i]
+        console.log(fiveDayData)
+
+        var date = fiveDayData.dt_txt.split(' ')[0]
+        console.log(date)
+        var weatherIcon = fiveDayData.weather[0].icon
+        console.log(weatherIcon)
+        var temp = fiveDayData.main.temp
+        console.log(temp)
+        var wind = fiveDayData.wind.speed
+        console.log(wind)
+        var humidity = fiveDayData.main.humidity
+        console.log(humidity)
+        var iconUrl = `http://openweathermap.org/img/w/${weatherIcon}.png`
+
+        var cardinnerHTML = `
+        <div class="card text-bg-primary mb-3" style="max-width: 11rem;">
+            <div class="card-header">${date}</div>
+            <div class="card-body">
+                <img src="${iconUrl}"/>
+                <p class="card-text">Temp: ${temp}°C</p>
+                <p class="card-text">Wind: ${wind} m/s</p>
+                <p class="card-text">Humidity: ${humidity}%</p>
+            </div>
+        </div>
+    `
+
+    cardContainer.innerHTML += cardinnerHTML
+    }
+}
